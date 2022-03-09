3d484e650e8f81506acf104fc43058d56887d334
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2022-03-09 11:51:57 +0530

    Added Header and ImgSlider

---
+++ src/components/Header.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return ( <>
+    <Nav>
+      <Logo src="./images/logo.svg" />
+      <NavMenu>
+          <a >
+            <img src="./images/home-icon.svg" alt='j' />
+            <span>HOME</span>
+          </a>
+          <a >
+            <img src="./images/search-icon.svg" alt='j' />
+            <span>SEARCH</span>
+          </a>
+          <a >
+            <img src="./images/watchlist-icon.svg" alt='j' />
+            <span>WATCHLIST</span>
+          </a>
+          <a >
+            <img src="./images/original-icon.svg" alt='j' />
+            <span>ORIGINALS</span>
+          </a>
+          <a >
+            <img src="./images/movie-icon.svg" alt='j' />
+            <span>MOVIE</span>
+          </a>
+          <a >
+            <img src="./images/series-icon.svg" alt='j' />
+            <span>SERIES</span>
+          </a>
+
+      </NavMenu>
+      <UserImg src='./images/viewers-marvel.png' />
+    </Nav>
+    </>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+overflow: hidden;
+height: 80px;
+width: 100%;
+background: #090b13;
+display: flex;
+align-items: center;
+padding: 0 36px;
+`
+
+const Logo = styled.img`
+width: 70px;
+
+`
+
+const NavMenu = styled.div`
+display: flex;
+align-items: center;
+flex: 1;
+a{
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+  cursor: pointer;
+  img{
+  height: 20px;
+}
+span{
+letter-spacing: 1.34px;
+font-size: 13px;
+margin-left: 5px;
+position: relative;
+&::after{
+  content: "";
+  height: 2px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  background: white;
+  width: 100%;
+  bottom: -4px;
+  transform-origin: left center;
+  transform: scaleX(0);
+  opacity: 0;
+}
+}
+
+&:hover{
+  span:after{
+    opacity: 1;
+    transform: scaleX(1);
+    transition:all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+  }
+}
+}
+
+`
+
+
+const UserImg = styled.img`
+width: 100px;
+height: 100px;
+border-radius: 50%;
+cursor: pointer;
+`
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
+const ImgSlider = () => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true
+  };
+  return (
+    <Sliderwrap {...settings}>
+  <Wrap>
+    <img src = "./images/slider-badag.jpg" alt="hey" />
+  </Wrap>
+  <Wrap>
+    <img src = "./images/slider-badging.jpg" alt="hey" />
+  </Wrap>
+  <Wrap>
+    <img src = "./images/slider-scale.jpg" alt="hey" />
+  </Wrap>
+  <Wrap>
+    <img src = "./images/slider-scales.jpg" alt="hey" />
+  </Wrap>
+
+    </Sliderwrap>
+  )
+}
+
+export default ImgSlider
+
+const Sliderwrap = styled(Slider)`
+margin-top:10px;
+ul li button{
+  &:before{
+    font-size: 10px;
+    color:rgb(150,158,171)
+  }
+}
+li.slick-active button:before{
+  color:white;
+}
+.slick-list{
+  overflow: visible;
+  /* margin: 0px 5px; */
+}
+button{
+  z-index: 1;
+}
+
+`
+
+const Wrap = styled.div`
+cursor: pointer;
+img{
+  border: 4px solid transparent;
+  height: 100%;
+  width: 100%;
+  border-radius: 6px;
+  box-shadow:rgb(0 0 0 /69%) 0px 26px 30px -10px,rgb(0 0 0 /73%) 0px 16px 10px -10px;
+  transition: 300ms;
+
+  &:hover{
+    border: 4px solid rgba(249,249,249,0.8);
+  }
+}
+
+`
